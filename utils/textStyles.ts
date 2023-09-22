0215ed802c3f8c8231f2fece72848e71c98233eb
André Silva <andreluiz.97@outlook.com>
2023-09-21 21:53:51 -0300

    feat: Text component

---
+++ utils/textStyles.ts
@@ -0,0 +1,45 @@
+type TextStyles = {
+  [key: string]: {
+    element: keyof JSX.IntrinsicElements,
+    classes: string
+  }
+};
+
+export const textStyles: TextStyles = {
+  h1: {
+    element: 'h1',
+    classes: 'font-mono font-bold text-6xl',
+  },
+  'h2-sans': {
+    element: 'h2',
+    classes: 'font-sans font-medium text-2xl',
+  },
+  'h2-mono': {
+    element: 'h2',
+    classes: 'font-mono font-semibold text-2xl uppercase',
+  },
+  h3: {
+    element: 'h3',
+    classes: 'font-sans font-medium text-base',
+  },
+  menu: {
+    element: 'span',
+    classes: 'font-mono font-bold text-base uppercase',
+  },
+  'body-1': {
+    element: 'p',
+    classes: 'font-sans font-normal text-base',
+  },
+  'body-2': {
+    element: 'p',
+    classes: 'font-sans font-normal text-sm',
+  },
+  'body-3': {
+    element: 'p',
+    classes: 'font-sans font-normal text-xs',
+  },
+  caption: {
+    element: 'span',
+    classes: 'font-mono font-normal text-xs uppercase',
+  },
+};
