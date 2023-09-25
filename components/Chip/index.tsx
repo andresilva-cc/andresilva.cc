50499292676d75542564742757654ad0bb5cb218
André Silva <andreluiz.97@outlook.com>
2023-09-25 12:43:11 -0300

    feat: Chip component

---
+++ components/Chip/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+import { Text } from '@/components/Text';
+
+export interface ChipProps {
+  children: ReactNode
+}
+
+export function Chip({ children }: ChipProps) {
+  return (
+    <Text variant="body-3" asChild>
+      <span className="px-1.5 py-1 text-primary-500 border border-primary-500 rounded">
+        { children }
+      </span>
+    </Text>
+  );
+}
