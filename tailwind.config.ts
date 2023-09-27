6bfb604a1a05c2447b92aee54819e617877568ea
André Silva <andreluiz.97@outlook.com>
2023-09-26 22:19:28 -0300

    chore: add tailwind-scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import scrollbar from 'tailwind-scrollbar';
 
 const config: Config = {
   content: [
@@ -43,6 +44,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbar],
 };
 export default config;
