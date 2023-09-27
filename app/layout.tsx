1cb7b67c7bff7d6240cc5cd753483a46d1493196
André Silva <andreluiz.97@outlook.com>
2023-09-27 19:08:35 -0300

    feat: base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
-import { firaCode, firaSans } from './fonts';
 import './globals.css';
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+import clsx from 'clsx';
+import { firaCode, firaSans } from './fonts';
 
 export const metadata: Metadata = {
   title: 'André Silva',
@@ -12,8 +15,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${firaSans.variable} ${firaCode.variable}`}>{children}</body>
+    <html lang="en" className="h-full">
+      <body
+        className={clsx(
+          firaSans.variable,
+          firaCode.variable,
+          'h-full flex flex-col px-4 md:px-8',
+        )}
+      >
+        <Header className="pt-4 md:pt-8 pb-8 md:pb-16" />
+        <main className="flex-grow">
+          { children }
+        </main>
+        <Footer className="py-8 md:py-16" />
+      </body>
     </html>
   );
 }
