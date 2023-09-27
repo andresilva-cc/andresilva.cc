7d805bef22b6a68ac3b8d79ad9eb82ffce9e007c
André Silva <andreluiz.97@outlook.com>
2023-09-27 19:55:42 -0300

    feat: increase layout padding for 2xl

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
+import { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import clsx from 'clsx';
 import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
-import clsx from 'clsx';
 import { firaCode, firaSans } from './fonts';
 
 export const metadata: Metadata = {
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: ReactNode
 }) {
   return (
     <html lang="en" className="h-full">
@@ -24,7 +25,7 @@ export default function RootLayout({
         )}
       >
         <Header className="pt-4 md:pt-8 pb-8 md:pb-16" />
-        <main className="flex-grow flex flex-col justify-center px-0 sm:px-12 md:px-24 lg:px-48">
+        <main className="flex-grow flex flex-col justify-center px-0 sm:px-12 md:px-24 lg:px-48 2xl:px-96">
           { children }
         </main>
         <Footer className="py-8 md:py-16" />
