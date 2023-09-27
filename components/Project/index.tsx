dded037f60954ae3d51d4c18b536c13558caef3d
André Silva <andreluiz.97@outlook.com>
2023-09-27 19:43:18 -0300

    fix: arrow icon size

--- components/Project/index.tsx
+++ components/Project/index.tsx
@@ -29,6 +29,7 @@ export function Project({
         { url && (
           <ArrowUpRight
             weight="bold"
+            size={16}
             className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5 transition-transform"
           />
         ) }
