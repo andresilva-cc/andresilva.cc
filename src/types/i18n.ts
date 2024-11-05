04f7e8e71fac965ff8494d8e55738a4845ba8e5a
André Silva <andreluiz.97@outlook.com>
2024-11-04 21:44:59 -0300

    chore: remove unused types

--- src/types/i18n.ts
+++
@@ -1,8 +0,0 @@
-import { useTranslations } from 'next-intl';
-import { RichTextProps } from '@/components/RichText';
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const t = useTranslations();
-export type TranslateFunction = typeof t;
-
-export type RichTextChildren = RichTextProps['children'];
