97bf6efaa1dc404e7ab53c38d0270dced912d3b7
André Silva <andreluiz.97@outlook.com>
2025-05-06 22:43:54 -0300

    feat: improve articles page

--- src/app/[locale]/articles/page.tsx
+++ src/app/[locale]/articles/page.tsx
@@ -1,8 +1,9 @@
-import { getTranslations } from 'next-intl/server';
+import { getLocale, getTranslations } from 'next-intl/server';
 
 import { Article } from '@/components/Article';
 import { Text } from '@/components/Text';
 import { useRepositories } from '@/hooks/useRepositories';
+import { Info } from '@phosphor-icons/react/dist/ssr/index';
 
 export async function generateMetadata() {
   const t = await getTranslations();
@@ -14,25 +15,38 @@ export async function generateMetadata() {
 
 export default async function Articles() {
   const t = await getTranslations();
+  const locale = await getLocale();
+
   const { articlesRepository } = useRepositories();
   const articles = await articlesRepository.getAll();
 
   return (
     <>
-      <div className="md:grid md:grid-cols-job md:gap-8">
-        <Text variant="h2-mono" element="h1" className="col-start-2">
+      <div className="pl-4">
+        <Text variant="h2-mono" element="h1">
           { t('articles.title') }
         </Text>
+
+        { locale !== 'en' && (
+        <div className="flex gap-2 mt-4 text-auxiliary-500">
+          <Info size={18} weight="bold" />
+          <Text variant="body-2">
+            { t('articles.englishOnly') }
+          </Text>
+        </div>
+        )}
       </div>
-      <ul className="flex flex-col gap-8 mt-8">
+      <ul className="flex flex-col gap-4 mt-6">
         { articles.map((article) => (
           <li key={article.id}>
             <Article
-              id={article.id}
               title={article.title}
-              description={article.description}
-              publishDate={article.readable_publish_date}
-              slug={article.slug}
+              publishedAt={article.published_at}
+              readingTime={article.reading_time_minutes}
+              commentsCount={article.comments_count}
+              reactionsCount={article.public_reactions_count}
+              url={article.url}
+              tags={article.tag_list}
             />
           </li>
         )) }
--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -1,34 +1,103 @@
-import { Text } from '@/components/Text';
-import { Link } from '@/i18n/routing';
-import type { Article as ArticleType } from '@/types/Article';
+import {
+  CalendarBlank, CaretDoubleRight, ChatCircleDots, Clock, Heart,
+} from '@phosphor-icons/react/dist/ssr';
 
-export interface ArticleProps extends Pick<ArticleType, 'id' | 'title' | 'description' | 'slug'> {
-  publishDate: string
+import { Text } from '@/components/Text';
+import type { Article as ArticleType } from '@/types/Article';
+import { useFormatter, useTranslations } from 'next-intl';
+import { Chip } from '../Chip';
+
+export interface ArticleProps {
+  title: ArticleType['title']
+  publishedAt: ArticleType['published_at']
+  readingTime: ArticleType['reading_time_minutes']
+  commentsCount: ArticleType['comments_count']
+  reactionsCount: ArticleType['public_reactions_count']
+  url: ArticleType['url']
+  tags: ArticleType['tag_list']
 }
 
 export function Article({
-  id, title, description, publishDate, slug,
+  title, readingTime, publishedAt, commentsCount, reactionsCount, url, tags,
 }: ArticleProps) {
-  const url = `/article/${id}/${slug}`;
+  const t = useTranslations();
+
+  const format = useFormatter();
+  const formattedPublishedAt = format.dateTime(new Date(publishedAt), {
+    dateStyle: 'short',
+  });
 
   return (
-    <div className="md:grid md:grid-cols-job md:gap-8">
-      <div className="md:text-right mb-2 md:mb-0">
-        <Text variant="caption" className="text-auxiliary-500">
-          { publishDate }
-        </Text>
+    <a
+      href={url}
+      target="_blank"
+      className="group flex flex-col gap-2 p-4 rounded-lg bg-primary-300/0 hover:bg-primary-300/5 active:bg-primary-300/10 select-none transition-colors hover:transition-none duration-300"
+    >
+      <Text
+        variant="h3"
+        className="text-secondary-500 hover:text-secondary-400 active:text-secondary-300 transition-colors"
+      >
+        { title }
+      </Text>
+
+      <Text
+        variant="body-2"
+        element="div"
+        className="flex flex-wrap leading-[13px] gap-x-6 md:gap-x-8"
+      >
+        <div className="flex gap-2 items-center">
+          <CalendarBlank weight="bold" size={16} />
+          { formattedPublishedAt }
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <Clock weight="bold" size={16} />
+          <span className="hidden md:inline">
+            { t('articles.readingTime', { minutes: readingTime }) }
+          </span>
+          <span className="md:hidden">
+            { readingTime }
+            {' '}
+            min
+          </span>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <ChatCircleDots weight="bold" size={16} />
+          <span className="hidden md:inline">
+            { t('articles.comments', { count: commentsCount }) }
+          </span>
+          <span className="md:hidden">
+            { commentsCount }
+          </span>
+        </div>
+
+        <div className="flex gap-2 items-center">
+          <Heart weight="bold" size={16} />
+          <span className="hidden md:inline">
+            { t('articles.reactions', { count: reactionsCount }) }
+          </span>
+          <span className="md:hidden">
+            { reactionsCount }
+          </span>
+        </div>
+      </Text>
+
+      <div className="flex flex-wrap gap-2 mt-2">
+        { tags.map((tag) => (
+          <Chip key={tag}>{ tag }</Chip>
+        ))}
       </div>
 
-      <div>
-        <Text variant="h3" asChild className="text-secondary-500 hover:text-secondary-400 active:text-secondary-300 transition-colors">
-          <Link href={url}>
-            { title }
-          </Link>
-        </Text>
-        <Text variant="body-2" element="div" className="mt-2">
-          { description }
-        </Text>
-      </div>
-    </div>
+      <Text
+        variant="caption"
+        className="flex gap-2 items-center mt-4 text-auxiliary-500 group-hover:translate-x-1 transition-transform"
+      >
+        { t('articles.readOn') }
+        {' '}
+        dev.to
+        <CaretDoubleRight weight="bold" size={14} />
+      </Text>
+    </a>
   );
 }
